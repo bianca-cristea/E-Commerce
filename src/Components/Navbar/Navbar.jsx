9227cc4329c56fdb2fecb11e88dadff9a5299626
bianca-cristea <bianca.cristea01@yahoo.com>
2024-02-03 18:44:15 +0200

    add commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -71,9 +71,20 @@ function Navbar() {
         </li>
       </ul>
       <div className="nav-login-cart">
-        <Link style={{ textDecoration: 'none' }} to="/login">
-          <button> Login</button>
-        </Link>
+        {localStorage.getItem('auth-token') ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem('auth-token');
+              window.location.replace('/');
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link style={{ textDecoration: 'none' }} to="/login">
+            <button> Login</button>
+          </Link>
+        )}
 
         <Link style={{ textDecoration: 'none' }} to="/cart">
           <img id="cart-icon" src={cart_icon} alt="cart" />
